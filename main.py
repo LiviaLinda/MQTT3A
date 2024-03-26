3ef7e640f948590a6a7370619b538bcc236face7
Livia <lcm.livia.mesquita@gmail.com>
2024-03-26 10:32:46 -0300

    aula 26/03

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ cliente.connect("broker.hivemq.com", 1883, 60)
 cliente.subscribe("aula3a")
 #cliente.loop_forever()
 
-cliente.loop_star()
+cliente.loop_start()
 while True:
     cliente.publish("aula3a", input("Escreva uma mensagem: "))
 cliente.loop_finish()
